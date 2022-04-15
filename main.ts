b55cfbd70b933b8abea61d0b84c08b2a1936e13e
Vladimir Lila <lila@skbkontur.ru>
2022-04-15 20:43:54 +0500

    fix

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import './main.css';
 
-var spawnerType = 0;
+var spawnerType = '#sample';
 var spawnerTypeCursor = '#sampleCursor';
 
 const getOffsetByType = (type) => {
@@ -11,19 +11,40 @@ const getOffsetByType = (type) => {
   }
 };
 
+let currentMode = true;
+
+checkModeButton();
+
 document.querySelector('#humanSpawner').onclick = () => {
   spawnerType = '#humanSpawner';
   spawnerTypeCursor = '#humanSpawnerCursor';
   console.log(spawnerType);
 };
 
+document.querySelector('#modeButton').onclick = () => {
+  currentMode = !currentMode;
+  checkModeButton();
+};
+
+const allPoints = [];
+// document.allPoints = allPoints;
+let previous = undefined;
 document.querySelector('#root').onclick = (event) => {
   let div = document.createElement('div');
   div.classList.add('asd');
+
   div.style.position = 'absolute';
   const offset = getOffsetByType(spawnerType);
   div.style.top = event.clientY - offset + 'px';
   div.style.left = event.clientX - offset + 'px';
+  if (previous != undefined) {
+    drawLine(event.clientX - offset, event.clientY, previous.style.left, previous.style.top);
+  }
+  previous = div;
+  allPoints.push({
+    x: event.clientX - offset,
+    y: event.clientY - offset,
+  });
   document.querySelector('#root').append(div);
 };
 
@@ -36,3 +57,48 @@ document.querySelector('#root').addEventListener('mousemove', (event) => {
   element.style.top = y - offset + 'px';
   element.style.left = x - offset + 'px';
 });
+
+const drawLine = (x1, y1, x2, y2) => {
+  var newDiv = document.createElement('div');
+  newDiv.style.position = 'absolute';
+  newDiv.innerHTML = `<svg width="900" height="900"><line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="black"/></svg>`;
+  document.querySelector('#root').append(newDiv);
+};
+function checkModeButton() {
+  if (currentMode === true) {
+    document.querySelector('#modeButton').textContent = 'Edit';
+  } else if (currentMode === false) {
+    document.querySelector('#modeButton').textContent = 'Play';
+  }
+}
+
+let currentPointTargetForPlayerIndex = 0;
+let playerPosition = { x: 22, y: 22 };
+const intervalId = setInterval(() => {
+  const player = document.querySelector('#player');
+  if (allPoints[currentPointTargetForPlayerIndex] == undefined) {
+    return;
+  }
+  const currentPoint = allPoints[currentPointTargetForPlayerIndex];
+  if (Math.abs(playerPosition.y - currentPoint.y) < 10 && Math.abs(playerPosition.x - currentPoint.x) < 10) {
+    currentPointTargetForPlayerIndex++;
+  }
+  if (Math.abs(playerPosition.y - currentPoint.y) >= 10) {
+    if (playerPosition.y - currentPoint.y < 0) {
+      playerPosition.y = playerPosition.y + 5;
+    } else {
+      playerPosition.y = playerPosition.y - 5;
+    }
+  }
+  if (Math.abs(playerPosition.x - currentPoint.x) >= 10) {
+    if (playerPosition.x - currentPoint.x < 0) {
+      playerPosition.x = playerPosition.x + 5;
+    } else {
+      playerPosition.x = playerPosition.x - 5;
+    }
+  }
+  player.style.top = playerPosition.y + 'px';
+  player.style.left = playerPosition.x + 'px';
+}, 50);
+
+// clearInterval(intervalId);
